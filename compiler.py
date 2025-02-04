5db251d5a08a92b8874e07075aa133f562d37309
Leonardo Filho <leo.veiga.filho@gmail.com>
2025-02-04 16:25:02 -0300

    Inicia a etapa de compilação no Linux

--- compiler.py
+++ compiler.py
@@ -4,6 +4,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("C:/Users/leove/Área de Trabalho/MESH/src/compiled/functions.pyx", compiler_directives={'language_level': "3"}),
+    # ext_modules=cythonize("C:/Users/leove/Área de Trabalho/MESH/src/compiled/functions.pyx", compiler_directives={'language_level': "3"}),
+    ext_modules=cythonize("/home/leonardo_filho/Área de Trabalho/MESH/src/compiled/functions.pyx", compiler_directives={'language_level': "3"}),
     include_dirs=[numpy.get_include()]
 )
