bf5e00c39bf86f5b2e518a2849bb7dd8b3d16f46
Leonardo Filho <leo.veiga.filho@gmail.com>
2025-02-03 23:13:02 -0300

    Refatora código após a etapa de compilação

--- src/compiled/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("src/compiled/to_compile.pyx", compiler_directives={'language_level': "3"})
-)
