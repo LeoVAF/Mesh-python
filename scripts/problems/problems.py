d38d81f26e5884d043d751fc4d48757732bcb5cf
Leonardo Filho <leo.veiga.filho@gmail.com>
2025-07-30 21:11:56 -0300

    Refactor problem definitions in problems.py to use pygmo's problem wrapper for ZDT and DTLZ problems, ensuring compatibility with new parameter structures. Update input validation for WFG problems to enforce required parameters and conditions. Adjust return values for problem functions to use numpy arrays for consistency.

--- scripts/problems/problems.py
+++ scripts/problems/problems.py
@@ -6,7 +6,7 @@ from .ZDT import zdt1, zdt2, zdt3, zdt4, zdt6,\
 
 from functools import partial
 from pymoo.problems.many.wfg import WFG1, WFG2, WFG3, WFG4, WFG5, WFG6, WFG7, WFG8, WFG9
-from pygmo import problem, wfg
+from pygmo import problem, dtlz, wfg, zdt
 from typing import Callable
 
 import numpy as np
@@ -30,38 +30,50 @@ def get_problem(name: str, n_var: int, n_obj: int, wfg_k: int | None = None):
 
   # Problem selection
   if name in {'zdt1', 'zdt2', 'zdt3', 'zdt6'}:
-    func = {'zdt1':zdt1, 'zdt2':zdt2, 'zdt3':zdt3, 'zdt6':zdt6}
-    return func[name], np.array([0.0]*n_var), np.array([1.0]*n_var)
-
+    func = {'zdt1': problem(zdt(prob_id=1, param=n_var)).fitness, 'zdt2': problem(zdt(prob_id=2, param=n_var)).fitness,
+            'zdt3': problem(zdt(prob_id=3, param=n_var)).fitness, 'zdt6': problem(zdt(prob_id=6, param=n_var)).fitness}
+    return func[name], np.zeros((n_var)), np.ones((n_var))
   elif name == 'zdt4':
-    return zdt4, np.array([0.0] + [-5.0]*(n_var-1)), np.array([1] + [5.0]*(n_var-1))
+    return problem(zdt(prob_id=4, param=n_var)).fitness, np.array([0.0] + [-5.0]*(n_var-1)), np.array([1] + [5.0]*(n_var-1))
 
   elif name in {'dtlz1', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6', 'dtlz7'}:
-    func = {'dtlz1':dtlz1, 'dtlz2':dtlz2, 'dtlz3':dtlz3, 'dtlz4':dtlz4, 'dtlz5':dtlz5, 'dtlz6':dtlz6, 'dtlz7':dtlz7}
-    return partial(func[name], n_obj=n_obj), np.array([0.0]*n_var), np.array([1.0]*n_var)
+    func = {'dtlz1':problem(dtlz(prob_id=1, dim=n_var, fdim=n_obj)).fitness, 'dtlz2':problem(dtlz(prob_id=2, dim=n_var, fdim=n_obj)).fitness,
+            'dtlz3':problem(dtlz(prob_id=3, dim=n_var, fdim=n_obj)).fitness, 'dtlz4':problem(dtlz(prob_id=4, dim=n_var, fdim=n_obj)).fitness,
+            'dtlz5':problem(dtlz(prob_id=5, dim=n_var, fdim=n_obj)).fitness, 'dtlz6':problem(dtlz(prob_id=6, dim=n_var, fdim=n_obj)).fitness,
+            'dtlz7':problem(dtlz(prob_id=7, dim=n_var, fdim=n_obj)).fitness,}
+    return func[name], np.zeros((n_var)), np.ones((n_var))
 
   elif name in {'wfg1', 'wfg4', 'wfg5', 'wfg6', 'wfg7', 'wfg8', 'wfg9'}:
     func = {'wfg1': problem(wfg(prob_id=1, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness, 'wfg4': problem(wfg(prob_id=4, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness,
             'wfg5': problem(wfg(prob_id=5, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness, 'wfg6': problem(wfg(prob_id=6, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness,
             'wfg7': problem(wfg(prob_id=7, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness, 'wfg8': problem(wfg(prob_id=8, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness,
             'wfg9': problem(wfg(prob_id=9, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness}
-    return func[name], np.array([0.0]*n_var), np.arange(1, n_var+1)
-
+    return func[name], np.zeros((n_var)), np.arange(1, n_var+1) * 2
   elif name in {'wfg2', 'wfg3'}:
     func = {'wfg2': problem(wfg(prob_id=2, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness, 'wfg3': problem(wfg(prob_id=3, dim_dvs=n_var, dim_obj=n_obj, dim_k=wfg_k)).fitness}
-    return func[name], np.array([0.0]*n_var), np.array(1, n_var+1)
+    return func[name], np.zeros((n_var)), np.arange(1, n_var+1) * 2
 
   else:
     raise ValueError(f"Problem {name} not found.")
 
 ######################################################## Pareto front #######################################################
 
-def get_pareto(name: str, N: int, n_var: int, n_obj: int) -> Callable:
+def get_pareto(name: str, N: int, n_var: int, n_obj: int, wfg_k: int | None = None) -> Callable:
   # Validation of inputs
   if name in {'zdt1', 'zdt2', 'zdt3', 'zdt4', 'zdt6'} and (n_obj != 2 or n_var < 2):
-    raise ValueError(f"Problem {name} only supports 2 objectives.")
-  if name in {'dtlz1', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6', 'dtlz7'} and n_var < n_obj:
-    raise ValueError(f"Problem {name} requires at least {n_obj} variables.")
+    raise ValueError(f'Problem {name} only supports 2 objectives.')
+  if name in {'dtlz1', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6', 'dtlz7'}:
+    if (n_var < n_obj):
+      raise ValueError(f'Problem {name} requires at least {n_obj} variables.')
+    if n_obj < 2:
+      ValueError(f'Problem {name} requires at least 2 objectives.')
+  if name in {'wfg1', 'wfg2', 'wfg3', 'wfg4', 'wfg5', 'wfg6', 'wfg7', 'wfg8', 'wfg9'}:
+    if wfg_k is None:
+      raise ValueError(f'For WFG problems, the parameter "k" is required.')
+    if (wfg_k % (n_obj - 1) != 0):
+      raise ValueError(f'For WFG problems, the parameter "k" must be a multiple of "n_obj" minus one.')
+    if name in {'wfg2', 'wfg3'} and ((n_var - wfg_k) % 2) != 0:
+      raise ValueError(f'For WFG problems, the number of distance-related variables (n_var - wfg_k) must be divisible by two.')
 
   # Pareto function selection
   pareto_set = {'zdt1':zdt1_pareto(N), 'zdt2':zdt2_pareto(N), 'zdt3':zdt3_pareto(N), 'zdt4':zdt4_pareto(N), 'zdt6':zdt6_pareto(N),
